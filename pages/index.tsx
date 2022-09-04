22b7eb55ad1337a212224921044f71804e6e4629
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-04 08:51:17 +0200

    nit

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,10 +121,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Stacks Dashboard</title>
         <link rel="icon" href="favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
       </Head>
       <Box p={4}>
         <Stack direction="row" spacing={4}>
