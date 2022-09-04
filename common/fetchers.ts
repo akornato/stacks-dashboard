6fc91e0cb4ed71401d26805ed192a0e8ccb9e1c4
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-04 20:15:00 +0200

    fix session fetchers

--- common/fetchers.ts
+++ common/fetchers.ts
@@ -1,21 +1,23 @@
-import { API_URL } from "./constants";
-
 export const saveSession = async (dehydratedState: string) => {
-  await fetch(API_URL + "/api/session/save", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ dehydratedState }),
-  });
+  try {
+    await fetch("/api/session/save", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ dehydratedState }),
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const destroySession = async () => {
   try {
-    await fetch(API_URL + "/api/session/destroy", {
+    await fetch("/api/session/destroy", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: null,
     });
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 };
