6ffc18cac7b4b1a9ae44bc1dd8739049385a2a02
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-04 08:45:48 +0200

    unsubscribe subscriptions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Head from "next/head";
 import { Box, Stack } from "@chakra-ui/react";
 import { WalletConnectButton } from "../components/WalletConnectButton";
@@ -7,7 +7,10 @@ import { TransactionsTable } from "../components/TransactionsTable";
 import { NetworkToggle } from "../components/NetworkToggle";
 import { useAuth, useAccount } from "@micro-stacks/react";
 import { getDehydratedStateFromSession } from "../common/session-helpers";
-import { connectWebSocketClient } from "@stacks/blockchain-api-client";
+import {
+  connectWebSocketClient,
+  StacksApiWebSocketClient,
+} from "@stacks/blockchain-api-client";
 import { unionBy } from "lodash";
 import type { NextPage, GetServerSidePropsContext } from "next";
 import type {
@@ -23,13 +26,17 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   };
 }
 
+type Subscription = Awaited<
+  ReturnType<StacksApiWebSocketClient["subscribeBlocks"]>
+>;
+
 const Home: NextPage = () => {
   const { isSignedIn } = useAuth();
   const { stxAddress } = useAccount();
   const [transactions, setTransactions] = useState<Transaction[] | undefined>();
+  const subscriptions = useRef<Subscription[]>();
 
   useEffect(() => {
-    console.log(isSignedIn, stxAddress);
     if (isSignedIn && stxAddress) {
       const getTransactions = async () => {
         const cachedTransactions: Transaction[] = await fetch(
@@ -72,30 +79,41 @@ const Home: NextPage = () => {
       getTransactions();
 
       connectWebSocketClient("wss://stacks-node-api.testnet.stacks.co/").then(
-        (client) => {
-          client.subscribeBlocks((event) =>
-            console.log("subscribeBlocks event", event)
-          );
-          client.subscribeAddressTransactions(stxAddress, async (event) => {
-            console.log("subscribeAddressTransactions event", event);
-            fetch(
-              `https://stacks-node-api.testnet.stacks.co/extended/v1/tx/${event.tx_id}`
-            )
-              .then((response) => response.json())
-              .then((transaction: Transaction) => {
-                console.log("transaction", transaction);
-                setTransactions((transactions) =>
-                  unionBy(
-                    [transaction],
-                    transactions,
-                    (transaction) => transaction.tx_id
-                  )
-                );
-              });
-          });
+        async (client) => {
+          subscriptions.current = [
+            await client.subscribeBlocks((event) =>
+              console.log("subscribeBlocks event", event)
+            ),
+            await client.subscribeAddressTransactions(
+              stxAddress,
+              async (event) => {
+                console.log("subscribeAddressTransactions event", event);
+                fetch(
+                  `https://stacks-node-api.testnet.stacks.co/extended/v1/tx/${event.tx_id}`
+                )
+                  .then((response) => response.json())
+                  .then((updatedTransaction: Transaction) => {
+                    console.log("updatedTransaction", updatedTransaction);
+                    setTransactions((transactions) =>
+                      unionBy(
+                        [updatedTransaction],
+                        transactions,
+                        (transaction) => transaction.tx_id
+                      )
+                    );
+                  });
+              }
+            ),
+          ];
         }
       );
     }
+
+    if (!isSignedIn) {
+      subscriptions.current?.forEach((sub) => sub.unsubscribe());
+    }
+
+    return () => subscriptions.current?.forEach((sub) => sub.unsubscribe());
   }, [isSignedIn, stxAddress]);
 
   return (
